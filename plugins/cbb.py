2503c76c205335a2e7b001937f5a452f4a207f63
iltamzumga <137275767+iltamzumga@users.noreply.github.com>
2024-04-07 20:25:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Main Channel : @prunwallah\n○ Support Bot : @prnwallahsupport_bot</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Main Channel : @Guftuguu\n○ Support Bot : @icecream_parlour</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
