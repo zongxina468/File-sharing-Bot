2e65174e3cfaac4f877da2f364a606161fe2502c
zongxina468 <137771833+zongxina468@users.noreply.github.com>
2024-05-08 12:34:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,15 +2,14 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-
+jamal_id = 5826834421
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Main Channel : @Guftuguu\n○ Support Bot : @icecream_parlour</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={jamal_id}'>This Person</a>\n○ Main Channel : @Guftuguu\n○ Support Bot : @icecream_parlour</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
